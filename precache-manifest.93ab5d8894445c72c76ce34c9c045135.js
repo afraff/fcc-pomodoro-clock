2af15861f22d0bcaae2dc97467f60f7b5921129c
Amy Rafferty <amyfrances.r@gmail.com>
2019-02-12 06:50:56 -0700

    Updates

---
+++ precache-manifest.93ab5d8894445c72c76ce34c9c045135.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ad5dc8ced7a11fe8830",
+    "url": "/fcc-pomodoro-clock/static/js/runtime~main.1ad5dc8c.js"
+  },
+  {
+    "revision": "005f34be3142d3f1097b",
+    "url": "/fcc-pomodoro-clock/static/js/main.005f34be.chunk.js"
+  },
+  {
+    "revision": "ca7f7ba5603c5e6c6274",
+    "url": "/fcc-pomodoro-clock/static/js/1.ca7f7ba5.chunk.js"
+  },
+  {
+    "revision": "005f34be3142d3f1097b",
+    "url": "/fcc-pomodoro-clock/static/css/main.3c09cea4.chunk.css"
+  },
+  {
+    "revision": "fa8d87ef99ba6219658152eaf5d126ac",
+    "url": "/fcc-pomodoro-clock/index.html"
+  }
+];
